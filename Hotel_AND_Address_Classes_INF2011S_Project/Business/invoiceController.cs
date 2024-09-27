7aeb773e399ba68f3106599dfcdba96e55f0b0ec
zaakirah236 <126486483+zaakirah236@users.noreply.github.com>
2024-09-27 22:48:49 +0200

    Update invoiceController.cs

--- Hotel_AND_Address_Classes_INF2011S_Project/Business/invoiceController.cs
+++ Hotel_AND_Address_Classes_INF2011S_Project/Business/invoiceController.cs
@@ -1,1 +1,27 @@
+using PhumlaKamnandiSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace PhumlaKamnandiSystem.Business
+{
+    public class InvoiceController
+    {
+        private BookingController bookingController;
+        private RoomController roomController;
+        private HotelController hotelController;
+        private GuestController guestController;
+
+        public InvoiceController()
+        {
+            bookingController = new BookingController();
+            roomController = new RoomController();
+            hotelController = new HotelController();
+            guestController = new GuestController();
+        }
+
+    }
