79d72b96c4e7b1c27b5d33706b0abf85605ed4d0
Ntuthuko-s04 <talentntuthuko1@gmail.com>
2024-09-29 12:30:15 +0200

    Add files via upload

---
+++ Hotel_AND_Address_Classes_INF2011S_Project/Business/Room.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhumlaKamnandiSystem.Business
+{
+
+    internal class Room
+    {
+        private int _roomNumber;
+        public enum Status{ 
+            vacant,
+            occupied
+        }
+        private int _hotelId;
+        private int _roomId;
+        public Status status;
+       
+
+        public int RoomNumber
+        {
+            get { return _roomNumber; }
+            set { _roomNumber = value; }
+        }
+        
+        public int RoomId
+        {
+            get { return _roomId; }
+            set { _roomId = value; }
+        }
+
+        public int HotelId
+        {
+            get { return _hotelId; }
+            set { _hotelId = value; }
+        }
+
+        public Status getStatus
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public Room() { }
+        public Room(int roomId, int roomNum, Status status, int hotelId)
+        {
+            _roomId = roomId;
+            _hotelId=hotelId;
+            _roomNumber = roomNum;
+            getStatus = status;
+        }
+    }
+}
